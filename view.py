04943ad3e931dcb2ad9e7825abca836888d0093c
Cyril-Meyer <contact@cyrilmeyer.eu>
2021-01-30 14:18:44 +0100

    add a function to save visualization as gif and add possibility to select axis along view

--- view.py
+++ view.py
@@ -14,9 +14,12 @@ def show_image(image):
     return 0
 
 
-def show_stack(stack, interval=50):
+def show_stack(stack, interval=50, axis=0):
     if len(stack.shape) != 3:
         return 1
+
+    if axis is not 0:
+        stack = np.moveaxis(stack, axis, 0)
 
     fig = plt.figure()
     slices = []
@@ -26,5 +29,24 @@ def show_stack(stack, interval=50):
 
     anim = animation.ArtistAnimation(fig, slices, interval=interval, repeat_delay=1000, blit=True)
     plt.show()
+
+    return 0
+
+
+def gif_stack(stack, filename, interval=50, axis=0):
+    if len(stack.shape) != 3:
+        return 1
+
+    if axis is not 0:
+        stack = np.moveaxis(stack, axis, 0)
+
+    fig = plt.figure()
+    slices = []
+    for z in range(stack.shape[0]):
+        s = plt.imshow(stack[z], cmap='gray')
+        slices.append([s])
+
+    anim = animation.ArtistAnimation(fig, slices, interval=interval, repeat_delay=1000, blit=True)
+    anim.save(filename)
 
     return 0
