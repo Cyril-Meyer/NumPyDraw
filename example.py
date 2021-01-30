acc7eccb64c0c7551b68066bd2866d765cfaa77a
Cyril-Meyer <contact@cyrilmeyer.eu>
2021-01-30 14:21:46 +0100

    update example.py and upload example gif of 3D spheroid

--- example.py
+++ example.py
@@ -5,9 +5,13 @@ import view
 import numpy as np
 
 X = np.zeros((256, 256))
-npd2d.ellipse(X, 1.0, (128, 128), (64, 8))
+npd2d.ellipse(X, 1.0, (128, 128), (64, 8), rotation=np.deg2rad(45))
 view.show_image(X)
 
 X = np.zeros((256, 256, 256))
-npd3d.spheroid(X, 1.0, (128, 224, 32), (100, 50, 20), rotation_xy=np.deg2rad(90//8))
-view.show_stack(X, interval=25)
+npd3d.spheroid(X, 1.0, (128, 224, 32), (100, 50, 20), rotation_xy=np.deg2rad(20))
+view.show_stack(X, interval=25, axis=0)
+
+X = np.zeros((256, 256, 256))
+npd3d.spheroid(X, 1.0, (128, 128+32, 128-32), (100, 40, 80), rotation_xy=np.deg2rad(45))
+view.gif_stack(X, "example/3D_spheroid.gif", interval=25, axis=2)
